678f6c687fe87f4b267bd2488eb0b7093012e3d1
Nikita Kochetov <nothsmple@mail.ru>
2022-06-23 08:26:20 +0300

    Model has been added.

---
+++ ToDoOrNotToDo/ChecklistItem.swift
@@ -0,0 +1,13 @@
+//
+//  ChecklistItem.swift
+//  ToDoOrNotToDo
+//
+//  Created by Татьяна Кочетова on 21.06.2022.
+//
+
+import Foundation
+
+class ChecklistItem {
+  var text = ""
+  var checked = false
+}
