ddc846ce65bcbb295a1dddd7bdc8e1541f82fcd8
Nikita Kochetov <nothsmple@mail.ru>
2022-06-23 09:01:55 +0300

    Swipe-to-delete has been added.

--- ToDoOrNotToDo/ChecklistViewController.swift
+++ ToDoOrNotToDo/ChecklistViewController.swift
@@ -44,6 +44,7 @@ class ChecklistViewController: UITableViewController {
 
           let item = ChecklistItem()
           item.text = "I am a new row"
+          item.checked = true
           items.append(item)
 
           let indexPath = IndexPath(row: newRowIndex, section: 0)
@@ -87,7 +88,13 @@ class ChecklistViewController: UITableViewController {
       tableView.deselectRow(at: indexPath, animated: true)
     }
 
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
 
+         items.remove(at: indexPath.row)
+
+         let indexPaths = [indexPath]
+         tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
 
     
     func configureCheckmark(
