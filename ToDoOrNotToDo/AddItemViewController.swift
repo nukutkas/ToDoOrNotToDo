6455d35e03b9530c771d478436521edb739401f3
Nikita Kochetov <nothsmple@mail.ru>
2022-06-25 18:53:27 +0300

    shouldChangeCharactersIn has been added.

--- ToDoOrNotToDo/AddItemViewController.swift
+++ ToDoOrNotToDo/AddItemViewController.swift
@@ -7,18 +7,52 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
-
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var doneBarButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.largeTitleDisplayMode = .never
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      textField.becomeFirstResponder()
+    }
     @IBAction func cancel(_ sender: Any) {
         navigationController?.popViewController(animated: true)
     }
     @IBAction func done(_ sender: Any) {
+        print("Contents of the text field: \(textField.text!)")
+        
         navigationController?.popViewController(animated: true)
     }
+    
+    // MARK: - Table View Delegates
+    override func tableView(
+      _ tableView: UITableView,
+      willSelectRowAt indexPath: IndexPath
+    ) -> IndexPath? {
+      return nil
+    }
+    
+    // MARK: - Text Field Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+          let stringRange = Range(range, in: oldText)!
+          let newText = oldText.replacingCharacters(
+            in: stringRange,
+            with: string)
+          if newText.isEmpty {
+            doneBarButton.isEnabled = false
+          } else {
+            doneBarButton.isEnabled = true
+          }
+        // doneBarButtonisEnabled = !newText.isEmpty
+          return true
+    }
+
     
 }
