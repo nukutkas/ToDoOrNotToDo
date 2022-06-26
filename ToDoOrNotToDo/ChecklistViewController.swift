8c68ea4e19c616dad106bb7338ce9d2cf13892c3
Nikita Kochetov <nothsmple@mail.ru>
2022-06-26 14:53:46 +0300

    didFinishEditing has been added.

--- ToDoOrNotToDo/ChecklistViewController.swift
+++ ToDoOrNotToDo/ChecklistViewController.swift
@@ -151,5 +151,18 @@ class ChecklistViewController: UITableViewController, AddItemViewControllerDeleg
         tableView.insertRows(at: indexPaths, with: .automatic)
         navigationController?.popViewController(animated:true)
     }
+    
+    func addItemViewController(
+      _ controller: AddItemViewController,
+      didFinishEditing item: ChecklistItem
+    ) {
+      if let index = items.firstIndex(of: item) {
+        let indexPath = IndexPath(row: index, section: 0)
+        if let cell = tableView.cellForRow(at: indexPath) {
+          configureText(for: cell, with: item)
+        }
+      }
+      navigationController?.popViewController(animated: true)
+    }
 }
 
