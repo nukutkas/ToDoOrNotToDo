f5296f22d37420d68ff2cb7007546f0a4044fe02
Nikita Kochetov <nothsmple@mail.ru>
2022-06-29 10:13:32 +0300

    willSelectRowAt has been added.

--- ToDoOrNotToDo/ListDetailViewController.swift
+++ ToDoOrNotToDo/ListDetailViewController.swift
@@ -58,5 +58,34 @@ class ListDetailViewController: UITableViewController, UITextFieldDelegate {
           didFinishAdding: checklist)
       }
     }
+    
+    // MARK: - Table View Delegates
+    override func tableView(
+      _ tableView: UITableView,
+      willSelectRowAt indexPath: IndexPath
+    ) -> IndexPath? {
+      return nil
+    }
+
+    // MARK: - Text Field Delegates
+    func textField(
+      _ textField: UITextField,
+      shouldChangeCharactersIn range: NSRange,
+      replacementString string: String
+    ) -> Bool {
+      let oldText = textField.text!
+      let stringRange = Range(range, in: oldText)!
+      let newText = oldText.replacingCharacters(
+        in: stringRange,
+        with: string)
+      doneBarButton.isEnabled = !newText.isEmpty
+      return true
+    }
+
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+      doneBarButton.isEnabled = false
+      return true
+    }
+
 
 }
