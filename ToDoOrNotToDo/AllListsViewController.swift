4e6d856412f487aa2e6c0be2a8c069535cbb2750
Nikita Kochetov <nothsmple@mail.ru>
2022-06-29 10:51:10 +0300

    List Detail View Controller Delegates have been added.

--- ToDoOrNotToDo/AllListsViewController.swift
+++ ToDoOrNotToDo/AllListsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AllListsViewController: UITableViewController {
+class AllListsViewController: UITableViewController, ListDetailViewControllerDelegate {
     
     let cellIdentifier = "ChecklistCell"
     
@@ -70,8 +70,46 @@ class AllListsViewController: UITableViewController {
       if segue.identifier == "ShowChecklist" {
         let controller = segue.destination as! ChecklistViewController
         controller.checklist = sender as? Checklist
-      }
+      } else if segue.identifier == "AddChecklist" {
+          let controller = segue.destination as! ListDetailViewController
+          controller.delegate = self
+        }
     }
+    
+    // MARK: - List Detail View Controller Delegates
+    func listDetailViewControllerDidCancel(
+      _ controller: ListDetailViewController
+    ) {
+      navigationController?.popViewController(animated: true)
+    }
+
+    func listDetailViewController(
+      _ controller: ListDetailViewController,
+      didFinishAdding checklist: Checklist
+    ) {
+      let newRowIndex = lists.count
+      lists.append(checklist)
+
+      let indexPath = IndexPath(row: newRowIndex, section: 0)
+      let indexPaths = [indexPath]
+      tableView.insertRows(at: indexPaths, with: .automatic)
+
+      navigationController?.popViewController(animated: true)
+    }
+
+    func listDetailViewController(
+      _ controller: ListDetailViewController,
+      didFinishEditing checklist: Checklist
+    ) {
+      if let index = lists.firstIndex(of: checklist) {
+        let indexPath = IndexPath(row: index, section: 0)
+        if let cell = tableView.cellForRow(at: indexPath) {
+          cell.textLabel!.text = checklist.name
+        }
+      }
+      navigationController?.popViewController(animated: true)
+    }
+
 
     
 }
