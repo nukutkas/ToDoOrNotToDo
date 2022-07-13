c0cfbe7244993c912197f56b69d969cfa85af570
Nikita Kochetov <nothsmple@mail.ru>
2022-07-13 13:16:13 +0300

    IconPickerViewController

---
+++ ToDoOrNotToDo/View Controllers/IconPickerViewController.swift
@@ -0,0 +1,44 @@
+//
+//  IconPickerViewController.swift
+//  ToDoOrNotToDo
+//
+//  Created by Татьяна Кочетова on 13.07.2022.
+//
+
+import UIKit
+
+protocol IconPickerViewControllerDelegate: AnyObject {
+    func iconPicker(_ picker: IconPickerViewController, didPick iconName: String)
+}
+
+class IconPickerViewController: UITableViewController {
+    weak var delegate: IconPickerViewControllerDelegate?
+    
+    let icons = [
+        "No icon", "Appointments", "Birthdays", "Chores",
+        "Drinks", "Folder", "Groceries", "Inbox", "Photos", "Trips"
+    ]
+    
+    // MARK: - Table View Delegates
+    override func tableView(
+      _ tableView: UITableView,
+      numberOfRowsInSection section: Int
+    ) -> Int {
+      return icons.count
+    }
+    
+    override func tableView(
+      _ tableView: UITableView,
+      cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(
+        withIdentifier: "IconCell",
+        for: indexPath)
+      let iconName = icons[indexPath.row]
+      cell.textLabel!.text = iconName
+      cell.imageView!.image = UIImage(named: iconName)
+      return cell
+    }
+
+
+}
