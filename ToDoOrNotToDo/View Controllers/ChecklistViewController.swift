543dfc7d2817a6ddb522bfa0a9ff13bf55bf6ce5
Nikita Kochetov <nothsmple@mail.ru>
2022-07-14 16:25:02 +0300

    configureText has been changed.

--- ToDoOrNotToDo/View Controllers/ChecklistViewController.swift
+++ ToDoOrNotToDo/View Controllers/ChecklistViewController.swift
@@ -57,8 +57,7 @@ class ChecklistViewController: UITableViewController, itemDetailViewControllerDe
         with item: ChecklistItem
     ) {
         let label = cell.viewWithTag(1000) as! UILabel
-        //label.text = item.text
-        label.text = "\(item.itemID): \(item.text)" 
+        label.text = item.text
     }
     
     // MARK: - Table View Data Source
